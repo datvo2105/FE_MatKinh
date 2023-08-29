58ac1213631578cfa6742592b4200ddd790cfc97
datvo2105 <contact.vgdat@gmail.com>
2023-08-29 15:15:14 +0700

    shop page

--- src/components/Shop/Content.jsx
+++ src/components/Shop/Content.jsx
@@ -1,31 +1,38 @@
-import React from 'react'
+import React from "react";
+import Sidebar from "./Sidebar";
+import ListProducts from "./ListProducts";
 
 const Content = () => {
   return (
     <>
       <div className="page-content">
-        <div class="collection-header">
-          <div class="collection-hero">
-            <div class="collection-hero__image">
+        <div className="collection-header">
+          <div className="collection-hero">
+            <div className="collection-hero__image">
               <img
-                class="blur-up lazyload"
+                className="blur-up lazyload"
                 data-src="assets/images/cat-women1.jpg"
                 src="assets/images/cat-women1.jpg"
                 alt="Women"
                 title="Women"
               />
             </div>
-            <div class="collection-hero__title-wrapper">
-              <h1 class="collection-hero__title page-width">
+            <div className="collection-hero__title-wrapper">
+              <h1 className="collection-hero__title page-width">
                 Shop Grid 4 Column
               </h1>
             </div>
           </div>
         </div>
-        
+        <div className="container">
+          <div className="row">
+            <Sidebar/>
+            <ListProducts/>
+          </div>
+        </div>
       </div>
     </>
   );
-}
+};
 
-export default Content
+export default Content;
